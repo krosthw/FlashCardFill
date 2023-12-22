6b954e5c20213af41f4f58171206a88ca23e683a
alessandro.costantini <alessandro.costantini@blmgroup.it>
2023-12-22 08:56:55 +0100

    git config core.ignorecase false

---
+++ components/navbar.tsx
@@ -0,0 +1,28 @@
+import Link from "next/link";
+
+import Container from "@/components/ui/container";
+
+
+
+const NavBar = async () => {
+
+    return (
+        <div className="border-b">
+            <Container>
+                <div className="relative px-4 sm:px-6 lg:px-8 flex h-16 items-center justify-between">
+                    <button className="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded">
+                        Prev
+                    </button>
+                    <button className="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded">
+                        LatoA
+                    </button>
+                    <button className="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded">
+                        Next
+                    </button>
+                </div>
+            </Container>
+        </div>
+    );
+};
+
+export default NavBar;
